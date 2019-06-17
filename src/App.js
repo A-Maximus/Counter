f152be9d6fdf8d05d6b1a2fb1fec715970f0103b
A-Maximus <awachuku86@gmail.com>
2019-06-17 16:41:36 -0400

    created a counter app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+// import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+class Counter extends Component {
+  constructor (props){
+    super(props);
+    this.state = {
+      counter: []
+    };
+  }
+
+
+  addCounter() {
+let addC= [...this.state.counter];
+addC.push(0);
+this.setState({counter: addC});
+  }
+
+
+
+  increment(i)  { 
+    console.log("incrementing has occured");
+    let c = [...this.state.counter];
+    c[i]++;
+    this.setState({ counter: c });
+  }
+
+
+  decrement(i)  { 
+    console.log("decrementing has occured");
+    let c = [...this.state.counter];
+    c[i].counter--;
+    this.setState({ counter: c });
+  }
+
+
+  
+  render() {
+    return (
+      
+      <div className= "main">
+          <button className= "add-counter" onClick= {this.addCounter.bind(this)}>Add counter</button>
+        { this.state.counter.map((c, i) =>
+          
+            <div key={i} className="choices">
+              <h1>{c}</h1>
+
+              <button className="increase" onClick= {this.increment.bind(this, i)}>Increment</button>
+              <button className="decrease">Decrement</button>
+            </div>
+          
+          
+          )
+        }
+       </div>
+       );
+       
+      }
+    }
+    
+    export default Counter;
+    // console.log(this.state.counter)
+    
