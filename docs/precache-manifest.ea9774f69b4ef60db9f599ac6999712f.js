d9d46947e368c1c38ba61da647382805a30fa98f
A-Maximus <awachuku86@gmail.com>
2019-06-17 17:26:27 -0400

    url stuff

---
+++ docs/precache-manifest.ea9774f69b4ef60db9f599ac6999712f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac54ec5528c8bffc359ea897a385ae70",
+    "url": "/counter/index.html"
+  },
+  {
+    "revision": "478254d1dfeadf60fb92",
+    "url": "/counter/static/css/main.a14688fe.chunk.css"
+  },
+  {
+    "revision": "1744db918d7799a6e6d6",
+    "url": "/counter/static/js/2.164617b5.chunk.js"
+  },
+  {
+    "revision": "478254d1dfeadf60fb92",
+    "url": "/counter/static/js/main.8b10e971.chunk.js"
+  },
+  {
+    "revision": "395f42efa7c50e4ddc67",
+    "url": "/counter/static/js/runtime~main.16ddd954.js"
+  }
+]);
